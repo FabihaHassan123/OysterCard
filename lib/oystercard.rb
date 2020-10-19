f4630a4a6dbe71580ceb73b5651c40afcab13e34
Shamarni <Smcfarlane926@gmail.com>
2020-10-19 16:01:24 +0100

    max balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,21 @@
 class Oystercard
-
+  # allows balance to be accessed outside of the class it is in.
   attr_accessor :balance
 
   DEFAULT_BALANCE = 0
-
-  def initialize(balance = DEFAULT_BALANCE)
+  MAX_BALANCE = 90
+  # sets variables for new instances of the class.
+  def initialize(balance = DEFAULT_BALANCE, max = MAX_BALANCE)
     @balance = balance
+    @max = max
   end
-
+  
   def view_balance
     puts balance
   end
 
+  def top_up(amount)
+    raise "maximum amount limit reached." if @balance + amount > @max
+    @balance += amount
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,4 +8,15 @@ describe Oystercard do
     end
   end
 
+  describe "#top_up" do
+    it "tops up the balance on oystercard" do
+      expect(subject.top_up(5)).to eq 5
+    end
+
+    it "caps the oystercard at £90" do
+      expect { subject.top_up(91) }.to raise_error("maximum amount limit reached.")
+    end
+  end
+
+
 end
